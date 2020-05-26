767c4107e488fd7d58a25ecd4487a69d7bfe98d2
Jen <jenhoang304@gmail.com>
2020-05-25 19:11:22 -0700

    update navbar in index.php

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <?php
 include('header.php');
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">Snacky</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto d-flex justify-content-end">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Store</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
-            </li>
-        </ul>
+<nav class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Snacky</h5>
+
+
+    <div class="nav nav-bar justify-content-end align-items-center">
+        <div class="nav-item ">
+            <a class="nav-link text-dark" href="#">Home</a>
+        </div>
+        <div class="nav-item">
+            <a class="nav-link text-dark" href="#">Store</a>
+        </div>
+        <div class="nav-item">
+            <a class="nav-link text-dark" href="#">Login</a>
+        </div>
+
 
     </div>
+
+    <span class="material-icons">
+        shopping_basket
+    </span>
+
 </nav>
+
 <div id="banner" class="container-fluid">
 
 </div>
