f3177a1f6f6778db1f4e68f5f1127af9525f1853
Jen <jenhoang304@gmail.com>
2020-05-25 17:23:45 -0700

    update index.php file

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 include('header.php');
 ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Snacky</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto d-flex justify-content-end">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Store</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
 <div id="banner" class="container-fluid">
 
 </div>
