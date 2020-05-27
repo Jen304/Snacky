33ed1152c06e090a6b38eeb2e5f4f1338b265690
Seiji Sagawa <seiji.rb.tr0327@gmail.comm>
2020-05-27 15:58:48 -0700

    Add insert data file to insert categories to our database

---
+++ database/insert_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO category (category_name)
+			   VALUES('Chocolate');
+               
+INSERT INTO category (category_name)
+			   VALUES('Salty');
+               
+INSERT INTO category (category_name)
+			   VALUES('Sweet');
+               
+INSERT INTO category (category_name)
+			   VALUES('Healthy');
+               
+INSERT INTO category (category_name)
+			   VALUES('Homemade');
+               
+INSERT INTO category (category_name)
+			   VALUES('Gluten-Free');
+               
+SELECT *
+FROM category;
