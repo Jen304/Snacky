7edc73e7c783a1ffd6d799fae5a179b824512ff2
Jen <jenhoang304@gmail.com>
2020-05-31 12:29:59 -0700

    create navbar file

---
+++ includes/nav_bar.php
@@ -0,0 +1,21 @@
+<nav class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow">
+
+    <img src="./images/logo.png" class="mr-md-auto " id-"logo" alt="logo">
+
+    <div class="nav nav-bar justify-content-end align-items-center">
+        <div class="nav-item ">
+            <a class="nav-link text-dark" href="#">Home</a>
+        </div>
+        <div class="nav-item">
+            <a class="nav-link text-dark" href="#">Store</a>
+        </div>
+        <div class="nav-item">
+            <a class="nav-link text-dark" href="#">Login</a>
+        </div>
+    </div>
+
+    <span class="material-icons">
+        shopping_basket
+    </span>
+
+</nav>
--- index.php
+++ index.php
@@ -7,23 +7,9 @@
 </head>
 
 <body>
-    <nav class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow">
-        <img src="../images/logo.png" class="mr-md-auto " id-"logo" alt="logo">
-        <div class="nav nav-bar justify-content-end align-items-center">
-            <div class="nav-item ">
-                <a class="nav-link text-dark" href="#">Home</a>
-            </div>
-            <div class="nav-item">
-                <a class="nav-link text-dark" href="#">Store</a>
-            </div>
-            <div class="nav-item">  
-                <a class="nav-link text-dark" href="#">Login</a>   
-            </div>
-        </div>
-        <span class="material-icons">
-        shopping_basket
-        </span>
-    </nav>
+    <?php
+        include('includes/nav_bar.php');
+    ?>
     <div id="banner" class="container-fluid d-flex align-items-center">
         <div id="greeting-quote" class="container">
             <h1 class="display-1">time for snack</h1>
