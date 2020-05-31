d046ef8dfd32bebcda31b2cc8bb23979c8bee6d6
Jen <jenhoang304@gmail.com>
2020-05-31 16:16:17 -0700

    update nav bar

--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -4,18 +4,34 @@
 
     <div class="nav nav-bar justify-content-end align-items-center">
         <div class="nav-item ">
-            <a class="nav-link text-dark" href="#">Home</a>
+            <a class="nav-link text-dark" href="/">Home</a>
         </div>
         <div class="nav-item">
-            <a class="nav-link text-dark" href="#">Store</a>
+            <a class="nav-link text-dark" href="/products/">Store</a>
         </div>
-        <div class="nav-item">
-            <a class="nav-link text-dark" href="#">Login</a>
-        </div>
+        <?php
+            if(empty($_SESSION['user_email'])){
+                echo '
+                <div class="nav-item">
+                <a class="nav-link text-dark" href="/user_login.php">Login</a>
+            </div>
+                ';
+            }else{
+                echo '
+                <div class="nav-item">
+                <a class="nav-link text-dark" href="#">'.$_SESSION['user_email'].'</a>
+            </div>';
+            }
+        ?>
+
+    </div>
+    <div class="nav-item">
+        <a class="nav-link text-dark" href="/cart/">
+            <span class="material-icons">
+                shopping_basket
+            </span></a>
     </div>
 
-    <span class="material-icons">
-        shopping_basket
-    </span>
+
 
 </nav>
